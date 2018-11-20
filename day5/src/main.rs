6c662c53f32600d6c907cde5cc368fc52b3eae87
Christian Blades <cblades@pindrop.com>
2018-11-20 17:01:58 -0500

    day5.2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,22 +1,26 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
 
 fn main() -> Result<(), std::io::Error> {
-    let file = File::open("input-day5")?;
-    let buf = BufReader::new(file);
+    let mut file = File::open("input-day5")?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)?;
 
     let nice_count: usize = buf
         .lines()
-        .filter_map(|ln| if is_nice(&ln.unwrap()) { Some(1) } else { None })
+        .filter_map(|ln| if is_nice(&ln) { Some(1) } else { None })
         .sum();
     println!("day5.1 {}", nice_count);
-
+    let day2_nice: usize = buf
+        .lines()
+        .filter_map(|ln| if day2(&ln) { Some(1) } else { None })
+        .sum();
+    println!("day5.2 {}", day2_nice);
     Ok(())
 }
 
 fn is_nice(input: &str) -> bool {
-    let did_i_stutter = stutters(&input);
+    let did_i_stutter = stutters(&input); // aa bb cc dd ee
     let vowels: usize = input
         .chars()
         .filter_map(|c| match c {
@@ -34,10 +38,6 @@ fn is_nice(input: &str) -> bool {
         .filter(|&&s| input.contains(s))
         .next()
         .is_some();
-
-    // println!("vowels: {}", vowels);
-    // println!("stutters: {}", did_i_stutter);
-    // println!("has_bad_set: {}", has_bad_set);
 
     !has_bad_set && vowels >= 3 && did_i_stutter
 }
@@ -64,10 +64,89 @@ fn stutters(input: &str) -> bool {
     stutters.next().is_some()
 }
 
+fn repeating_pair(input: &str) -> bool {
+    let seed = input[0..1].to_string();
+    let pairs: Vec<String> = input[1..]
+        .chars()
+        .scan(seed, |prev, c| {
+            let this = format!("{}{}", prev, c).to_string();
+            *prev = c.to_string();
+            Some(this)
+        })
+        .collect();
+
+    let count: usize = pairs
+        .iter()
+        .enumerate()
+        .filter_map(|(i, x)| {
+            let matches = match_index(x, &pairs);
+            if matches.len() < 2 {
+                return None;
+            }
+
+            matches
+                .iter()
+                .filter_map(|&match_i| {
+                    if match_i == i || match_i == i + 1 || match_i == i.saturating_sub(1) {
+                        None
+                    } else {
+                        Some(1)
+                    }
+                })
+                .next()
+        })
+        .sum();
+
+    count > 0
+}
+
+fn match_index(needle: &str, haystack: &[String]) -> Vec<usize> {
+    haystack
+        .iter()
+        .enumerate()
+        .filter_map(|(i, x)| if x == needle { Some(i) } else { None })
+        .collect()
+}
+
 #[derive(Debug)]
 enum Stutter {
     Double(char),
     None,
+}
+
+fn aba(input: &str) -> bool {
+    let seed = input[0..2].to_string();
+    let mut aba_iter = input[2..]
+        .chars()
+        .scan(seed, |prev, c| {
+            let this = format!("{}{}", prev, c);
+            *prev = this[1..3].to_string();
+            let a = this.get(0..1);
+            let x = this.get(2..3);
+            if a == x {
+                Some(Aba::Aba(c))
+            } else {
+                Some(Aba::None)
+            }
+        })
+        .filter(|x| match x {
+            Aba::Aba(_) => true,
+            Aba::None => false,
+        })
+        .take(1);
+    aba_iter.next().is_some()
+}
+
+#[derive(Debug)]
+enum Aba {
+    Aba(char),
+    None,
+}
+
+fn day2(input: &str) -> bool {
+    let sep_pattern = aba(input);
+    let rep_pair = repeating_pair(input);
+    sep_pattern && rep_pair
 }
 
 #[cfg(test)]
@@ -86,5 +165,29 @@ mod test {
     #[test]
     fn test_stutters() {
         assert!(stutters("aa"));
+    }
+
+    #[test]
+    fn test_aba() {
+        assert!(aba("aba"));
+        assert!(aba("aaa"));
+        assert!(!aba("aab"));
+        assert!(!aba("abba"));
+    }
+
+    #[test]
+    fn test_day2() {
+        assert!(day2("qjhvhtzxzqqjkmpb"));
+        assert!(day2("xxyxx"));
+        assert!(!day2("uurcxstgmygtbstg"));
+        assert!(!day2("ieodomkazucvgmuy"));
+    }
+
+    #[test]
+    fn test_repeating_pair() {
+        assert!(repeating_pair("aaaa"));
+        assert!(repeating_pair("aabcdefaa"));
+        assert!(!repeating_pair("abc"));
+        assert!(!repeating_pair("aaa"));
     }
 }
